de33a723e95315df9161e29f2f343b29afa7cd3e
Ahmed Bechir Mezhoud <ahmedbechir.mezhoud@insat.ucar.tn>
2026-02-15 13:24:18 +0100

    feat(component): add EpisodeCard component for displaying episode details

---
+++ app/components/EpisodeCard.tsx
@@ -0,0 +1,61 @@
+import { memo } from "react"
+import { Pressable, View } from "react-native"
+
+import { EpisodeUi } from "@/services/api/types"
+import { getEpLabel } from "@/utils/episodes"
+
+import { Text } from "./Text"
+
+function cardStyle({ pressed }: { pressed: boolean }) {
+  return [styles.card, pressed ? styles.cardPressed : undefined]
+}
+
+function EpisodeCard({ episode, onPress }: { episode: Partial<EpisodeUi>; onPress?: () => void }) {
+  return (
+    <Pressable accessibilityRole="button" style={cardStyle} onPress={() => onPress?.()}>
+      <Text text={getEpLabel(episode.episode)} style={styles.epCode} />
+
+      <View style={styles.cardBody}>
+        <Text text={episode.name} style={styles.epName} />
+        <Text text={episode.airDate} style={styles.epDate} />
+      </View>
+    </Pressable>
+  )
+}
+
+const styles = {
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#2B2545",
+    borderRadius: 16,
+    borderColor: "#735C92",
+    borderWidth: 1,
+    marginBottom: 12,
+  },
+  cardPressed: {
+    backgroundColor: "#2E2E4D",
+  },
+  epCode: {
+    color: "#735C92",
+    fontWeight: "bold" as const,
+    marginRight: 16,
+    fontSize: 32,
+  },
+  cardBody: {
+    flexDirection: "column" as const,
+    flex: 1,
+  },
+  epName: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  epDate: {
+    color: "#A6A6C7",
+    fontSize: 12,
+    marginTop: 4,
+  },
+}
+
+export default memo(EpisodeCard)
